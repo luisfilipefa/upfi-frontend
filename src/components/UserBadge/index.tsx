75615e10832bdb8965d8848271a55388193d33c1
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-28 13:19:27 -0300

    update user badge

--- src/components/UserBadge/index.tsx
+++ src/components/UserBadge/index.tsx
@@ -1,25 +1,40 @@
-import { Avatar, Container, Info } from "./styles";
+import { Avatar, Container, Info, Menu, MenuItem, MenuList } from "./styles";
+import { FiChevronDown, FiX } from "react-icons/fi";
 
 import { IUser } from "../../interfaces/user.interface";
+import { handleSignOut } from "../../context/AuthContext";
+import { useState } from "react";
 
 interface UserBadgeProps {
   user: IUser;
 }
 
 export function UserBadge({ user }: UserBadgeProps) {
+  const [showSignOut, setShowSignOut] = useState(false);
+
   return (
     <Container>
-      <Info>
+      <Info align="flex-start">
         <span>{user.name}</span>
         <span>{user.email}</span>
       </Info>
-      <Avatar
-        src={user.avatar}
-        alt={user.name}
-        objectFit="cover"
-        width="50px"
-        height="50px"
-      />
+      <Info align="center">
+        <Avatar
+          src={user.avatar}
+          alt={user.name}
+          objectFit="cover"
+          width="50px"
+          height="50px"
+        />
+        <FiChevronDown onClick={() => setShowSignOut(!showSignOut)} />
+        {showSignOut && (
+          <Menu>
+            <MenuList>
+              <MenuItem onClick={handleSignOut}>Sign Out</MenuItem>
+            </MenuList>
+          </Menu>
+        )}
+      </Info>
     </Container>
   );
 }
--- src/components/UserBadge/styles.ts
+++ src/components/UserBadge/styles.ts
@@ -1,23 +1,65 @@
 import Image from "next/image";
 import styled from "styled-components";
 
+interface InfoProps {
+  align: string;
+}
+
 export const Container = styled.div`
   display: flex;
   align-items: center;
   gap: 1rem;
+
+  height: 100%;
+
+  position: relative;
 `;
 
-export const Info = styled.div`
+export const Info = styled.div<InfoProps>`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: ${(props) => props.align};
   gap: 0.3rem;
 
   span {
     font-size: 0.8rem;
   }
+
+  svg {
+    cursor: pointer;
+  }
 `;
 
 export const Avatar = styled(Image)`
   border-radius: 50%;
+
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+`;
+
+export const Menu = styled.div`
+  position: absolute;
+  top: 110%;
+  right: 0;
+
+  width: 100%;
+
+  padding: 1rem;
+  border-radius: 0.5rem;
+
+  background: var(--gray-800);
+
+  z-index: 1;
+`;
+
+export const MenuList = styled.ul`
+  list-style: none;
+`;
+
+export const MenuItem = styled.li`
+  cursor: pointer;
 `;
