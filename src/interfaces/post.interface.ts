125a5853bd1d8565cc321e3233141744b47d5a2f
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-27 23:46:58 -0300

    add signin fn to signin form

--- src/interfaces/post.interface.ts
+++ src/interfaces/post.interface.ts
@@ -1,10 +1,10 @@
-import { User } from "./user.interface";
+import { IUser } from "./user.interface";
 
-export interface Post {
+export interface IPost {
   id: string;
   title: string;
   image: string;
   likes: string[];
-  author: Pick<User, "id" | "avatar" | "custom_username">;
+  author: Pick<IUser, "id" | "avatar" | "custom_username">;
   created_at: string;
 }
