dd28c000be731450030be81347b3461b673d994d
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-29 17:52:15 -0300

    update api config

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import { parseCookies } from "nookies";
 let cookies = parseCookies();
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || "http://localhost:3333",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3333",
   headers: {
     Authorization: `Bearer ${cookies["upfi.token"]}`,
   },
